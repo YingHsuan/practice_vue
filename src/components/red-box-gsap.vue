08607348cd88d362b7bbd30a7e5fffeca6b7d620
Lin Ying-Hsuan <wish7912@gmail.com>
2018-10-23 20:41:48 +0800

    add gsap

---
+++ src/components/red-box-gsap.vue
@@ -0,0 +1,19 @@
+<template>
+  <div ref="box" class="box"></div>
+</template>
+
+<script>
+import { TimelineLite } from "gsap";
+export default {
+  mounted() {
+    const { box } = this.$refs;
+    const timeline = new TimelineLite();
+
+    timeline.to(box, 1, { x: 200, rotation: 90 });
+  }
+};
+</script>
+
+<style>
+/* styles emitted */
+</style>
